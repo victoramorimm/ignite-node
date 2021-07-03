08cb76b8e3ba268ddd60692f3c339c937c0500f6
Victor Amorim <victorvmrgamer@gmail.com>
2021-07-02 23:32:40 -0300

    refactor: move all the responsibility of creating a category to a service

--- chapter2/rentalx/src/routes/categories.routes.ts
+++ chapter2/rentalx/src/routes/categories.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { Category } from "../model/Category";
 import { CategoriesRepository } from "../repositories/CategoriesRepository";
+import CreateCategoryService from "../services/CreateCategoryService";
 
 const categoriesRoutes = Router();
 const categoriesRepository = new CategoriesRepository();
@@ -8,18 +9,11 @@ const categoriesRepository = new CategoriesRepository();
 categoriesRoutes.post("/", (request, response) => {
   const { name, description } = request.body;
 
-  const categoryAlreadyExists = categoriesRepository.findByName(name);
+  const createCategoryService = new CreateCategoryService(categoriesRepository);
 
-  if (categoryAlreadyExists) {
-    return response.status(400).json({ error: "Category already exists!" });
-  }
+  createCategoryService.execute({ name, description });
 
-  const category = categoriesRepository.create({
-    name,
-    description,
-  });
-
-  return response.status(201).json({ category });
+  return response.status(201).send();
 });
 
 categoriesRoutes.get("/", (request, response) => {
