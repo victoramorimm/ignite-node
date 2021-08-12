43555629cef6249d8321e4bc0f40891f96d5c5fb
Victor Amorim <victorvmrgamer@gmail.com>
2021-08-12 15:27:27 -0300

    feat: change the throw new Error to throw new AppError

--- chapter2/rentalx/src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
+++ chapter2/rentalx/src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -2,6 +2,7 @@ import { inject, injectable } from "tsyringe";
 import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 import { hash } from "bcryptjs";
+import { AppError } from "../../../../errors/AppError";
 
 @injectable()
 class CreateUserUseCase {
@@ -16,10 +17,10 @@ class CreateUserUseCase {
     password,
     driver_license,
   }: ICreateUserDTO): Promise<void> {
-    const userAlreadyExists = await this.usersRepository.findByEmail(email)
+    const userAlreadyExists = await this.usersRepository.findByEmail(email);
 
     if (userAlreadyExists) {
-      throw new Error('User already exists')
+      throw new AppError("User already exists");
     }
 
     const passwordHash = await hash(password, 8);
